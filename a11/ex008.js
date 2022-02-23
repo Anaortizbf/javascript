24681a103dd24f60cb5a8026fef1574b22ab2276
Ana Ortiz Bomfim <anaortizroot@gmail.com>
2022-02-23 16:40:18 -0300

    update ex008.js
    Criando teste de velocidade para multa.

---
+++ a11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 50.5
+console.log(`A velocidade do seu carro é ${vel}Km/h`)
+if (vel >60) { //condição simples: porque não tem se não
+    console.log(`Você ultrapassou a velocidade permitida. MULTADO!`)
+}
+console.log(`Dirija sempre usando cinto de segurança!`)
