e9e7d4e04e6b59623d3edee7b272cc1f95c69f00
Ana Ortiz Bomfim <anaortizroot@gmail.com>
2022-02-26 20:34:18 -0300

    create ex011.js

---
+++ a12/ex011.js
@@ -0,0 +1,8 @@
+var idade = 16 //criada a variável idade
+if (idade < 16) { //se a idade for menor do que 16
+    console.log('Não vota') //não vota
+} else if (idade < 18) { //se a idade for  menor do que 18...
+    console.log('Voto opcional')
+} else {
+    console.log('Voto obrigatório')
+}
