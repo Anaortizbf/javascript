2880784ae9ae3df7901de685a9d9594886c094b4
Ana Ortiz Bomfim <anaortizroot@gmail.com>
2022-03-03 00:45:18 -0300

    create testes.js

---
+++ a13/testes.js
@@ -0,0 +1,24 @@
+/*do WHILE*/
+var c = 1
+do {
+    console.log(`Passo ${c}`)
+    c++
+} while (c <= 6)
+
+
+/* WHILE
+var c = 1
+while (c <= 6) {
+    console.log('Tudo bem?')
+    c++ //igual c recebe c + 1. Era 1, vai passar a valer 2
+}*/
+
+
+/* MANEIRA ERRADA
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+*/
