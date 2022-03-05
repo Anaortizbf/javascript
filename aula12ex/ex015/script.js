09649b3020e08f17e65293bd87a3264757e36daa
Ana Ortiz Bomfim <anaortizroot@gmail.com>
2022-03-05 12:16:16 -0300

    .

---
+++ aula12ex/ex015/script.js
@@ -0,0 +1,7 @@
+function verificar() {
+    var data = new Date()
+    var ano = data.getFullYear()
+    var fano = document.getElementById('txtano')
+    var res = document.querySelector('div#res')
+    if (fano.ariaValueMax.lenght == 0 )
+}
